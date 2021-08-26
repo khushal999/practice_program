db1669ca5487eb39e37ffc94f3a6495cf4e2e4c1
khushal999 <89569161+khushal999@users.noreply.github.com>
2021-08-26 12:38:28 +0530

    Update return_multi_val.c

--- return_multi_val.c
+++ return_multi_val.c
@@ -12,7 +12,6 @@ void arith(int val1, int val2, int *add, int *sub)
 int main()
 {
 	int val1=0, val2=0, add=0, sub=0;
-	int *ptr1= &add, *ptr2= &sub;
 
 	printf("\n Enter number 1:: ");
 	scanf("%d",&val1);
@@ -20,10 +19,10 @@ int main()
 	printf("\n Enter number 2:: ");
 	scanf("%d",&val2);
 	
-	arith(val1, val2, ptr1, ptr2);
+	arith(val1, val2, &add, &sub);
 
-	printf("\n Addition is    : %d ",*ptr1);
-	printf("\n Subtraction is : %d ",*ptr2);	
+	printf("\n Addition is    : %d ",add);
+	printf("\n Subtraction is : %d ",sub);	
 
 	return 0;
 }
